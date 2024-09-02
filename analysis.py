3a8075015aca75e1d37b4b481e1dc038d89d396a
Ryan Li <ryjli1120@gmail.com>
2024-09-02 17:07:47 -0400

    Added print function and commented out code

--- analysis.py
+++ analysis.py
@@ -3,13 +3,15 @@ import pandas as pd
 
 df = pd.read_csv('https://data.cms.gov/sites/default/files/2023-04/67157de9-d962-4af0-bf0e-3578b3afec58/inpatient.csv', sep=';')
 
-df.columns
+print(df.columns)
 
 
-print(data.head())
+# print(data.head())
 
-icd_codes = data['ICD_CODE']
-drg_codes = data['DRG_CODE']
-hcpcs_codes = data['HCPCS_CODE']
+# icd_codes = data['ICD_CODE']
+# drg_codes = data['DRG_CODE']
+# hcpcs_codes = data['HCPCS_CODE']
 
 #Struggled to print each column in a separeate line. Was difficult to see all column names available. 
+
+#This function will print hello world
